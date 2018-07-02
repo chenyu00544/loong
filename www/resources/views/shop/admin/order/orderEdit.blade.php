4248de301dce414a55c2700e1b73446f92012e95
chenyu00544 <chenyu00544@163.com>
2018-07-02 21:21:54 +0800

    update

--- www/resources/views/shop/admin/order/orderEdit.blade.php
+++ www/resources/views/shop/admin/order/orderEdit.blade.php
@@ -9,35 +9,37 @@
         <div class="content">
             <div class="flexilist">
                 <div class="stepflex">
-                    <dl class="first cur" data-step="1">
+                    <dl class="first cur">
                         <dt class="cursor">1</dt>
                         <dd class="s-text">提交订单</dd>
                         @if(!empty($order->add_time))
                             <dd class="s-time">{{date('Y-m-d H:i:s', $order->add_time)}}</dd>
                         @endif
                     </dl>
-                    <dl class="" data-step="2">
+                    <dl class="@if($order->pay_status == 2) cur @endif">
                         <dt class="cursor">2</dt>
                         <dd class="s-text">支付订单</dd>
-                        @if(!empty($order->pay_time))
+                        @if($order->pay_status == 0 || $order->pay_status == 1)
+                            <dd class="s-time">未付款</dd>
+                        @elseif($order->pay_status == 2 && !empty($order->pay_time))
                             <dd class="s-time">{{date('Y-m-d H:i:s', $order->pay_time)}}</dd>
                         @endif
                     </dl>
-                    <dl class="" data-step="3">
+                    <dl class="@if($order->shipping_status == 1) cur @endif">
                         <dt class="cursor">3</dt>
                         <dd class="s-text">商家发货</dd>
                         @if(!empty($order->shipping_time))
                             <dd class="s-time">{{date('Y-m-d H:i:s', $order->shipping_time)}}</dd>
                         @endif
                     </dl>
-                    <dl class="last" data-step="4">
+                    <dl class="last @if($order->shipping_status == 2) cur @endif">
                         <dt class="cursor">4</dt>
                         <dd class="s-text">确认收货</dd>
                         @if(!empty($order->confirm_take_time))
                             <dd class="s-time">{{date('Y-m-d H:i:s', $order->confirm_take_time)}}</dd>
                         @endif
                     </dl>
-                    <dl class="last" data-step="4">
+                    <dl class="last @if($order->shipping_status == 9) cur @endif">
                         <dt class="cursor">5</dt>
                         <dd class="s-text">评价</dd>
                         @if(!empty($order->pay_time))
@@ -52,12 +54,215 @@
                           class="form-horizontal">
                         {{csrf_field()}}
 
-                        <div class="step step-one" ectype="step" data-step="1" style="">
+                        <div class="order-step clearfix mar-bt-50">
                             <div class="step-title">
                                 <i class="ui-step"></i>
                                 <h3>基本信息</h3>
                             </div>
+                            <div class="section">
+                                <dl>
+                                    <dt>订单号：</dt>
+                                    <dd>2018070220185782655</dd>
+                                    <dt>订单来源：</dt>
+                                    <dd>PC</dd>
+                                </dl>
+                                <dl>
+                                    <dt>购货人：</dt>
+                                    <dd>chenyu</dd>
+                                    <dt>订单状态：</dt>
+                                    <dd>PC</dd>
+                                </dl>
+                                <dl>
+                                    <dt>支付方式：</dt>
+                                    <dd>银行汇款/转帐</dd>
+                                    <dt>配送方式：</dt>
+                                    <dd>顺丰速运</dd>
+                                </dl>
+                                <dl>
+                                    <dt>下单时间：</dt>
+                                    <dd>2018-07-02 20:18:01</dd>
+                                    <dt>付款时间：</dt>
+                                    <dd>未付款</dd>
+                                </dl>
+                                <dl>
+                                    <dt>发货时间：</dt>
+                                    <dd>未发货</dd>
+                                    <dt>发货单号：</dt>
+                                    <dd>未发货</dd>
+                                </dl>
+                                <dl>
+                                    <dt>自动确认收货时间：</dt>
+                                    <dd>15 天</dd>
+                                    <dt></dt>
+                                    <dd></dd>
+                                </dl>
+                            </div>
+                        </div>
 
+                        <div class="order-step clearfix mar-bt-50">
+                            <div class="step-title">
+                                <i class="ui-step"></i>
+                                <h3>收货人信息</h3>
+                            </div>
+                            <div class="section">
+                                <dl>
+                                    <dt>订单号：</dt>
+                                    <dd>2018070220185782655</dd>
+                                    <dt>订单来源：</dt>
+                                    <dd>PC</dd>
+                                </dl>
+                                <dl>
+                                    <dt>购货人：</dt>
+                                    <dd>chenyu</dd>
+                                    <dt>订单状态：</dt>
+                                    <dd>PC</dd>
+                                </dl>
+                                <dl>
+                                    <dt>支付方式：</dt>
+                                    <dd>银行汇款/转帐</dd>
+                                    <dt>配送方式：</dt>
+                                    <dd>顺丰速运</dd>
+                                </dl>
+                                <dl>
+                                    <dt>下单时间：</dt>
+                                    <dd>2018-07-02 20:18:01</dd>
+                                    <dt>付款时间：</dt>
+                                    <dd>未付款</dd>
+                                </dl>
+                                <dl>
+                                    <dt>发货时间：</dt>
+                                    <dd>未发货</dd>
+                                    <dt>发货单号：</dt>
+                                    <dd>未发货</dd>
+                                </dl>
+                                <dl>
+                                    <dt>自动确认收货时间：</dt>
+                                    <dd>15 天</dd>
+                                    <dt></dt>
+                                    <dd></dd>
+                                </dl>
+                            </div>
+                        </div>
+
+                        <div class="order-step clearfix">
+                            <div class="step-title">
+                                <i class="ui-step"></i>
+                                <h3>商品信息</h3>
+                            </div>
+                            <div class="order-goods">
+                                <table class="table table-hover table-condensed">
+                                    <thead>
+                                    <tr>
+                                        <th width="25%">商品名称 [ 品牌 ]</th>
+                                        <th width="8%" class="text-center">仓库名称</th>
+                                        <th width="7%" class="text-center">货号</th>
+                                        <th width="7%" class="text-center">条形码</th>
+                                        <th width="7%" class="text-center">货品号</th>
+                                        <th width="6%" class="text-center">价格</th>
+                                        <th width="5%" class="text-center">赠送积分</th>
+                                        <th width="5%" class="text-center">数量</th>
+                                        <th width="8%" class="text-center">属性</th>
+                                        <th width="5%" class="text-center">库存</th>
+                                        <th width="8%" class="text-center">小计</th>
+                                        <th width="7%" class="text-center">操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td class="td-product">
+                                            <div>
+                                                <div class="img fl">
+                                                    <img width="80" src="http://localhost/images/201703/thumb_img/0_thumb_G_1490155064450.jpg">
+                                                </div>
+                                                <div class="product-info">
+                                                    <div class="name">
+                                                        <a href="705" target="_blank">绿联 数据线安卓高速单头加长手机通用华为魅族小米2a快充电器2米 铝合金外壳+纯铜芯，充电快寿命长
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td class="text-center">
+                                            <a type="button" href="{{url('admin/users/info/')}}"
+                                               class="btn btn-info btn-edit btn-sm mar-all-5">生成发货单</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="12">
+                                            <div class="order-total fr">
+                                                <strong>合计：</strong>
+                                                <span class="red ft-16"><em>¥</em>18.00</span>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="order-step clearfix mar-bt-50">
+                            <div class="step-title">
+                                <i class="ui-step"></i>
+                                <h3>费用信息</h3>
+                            </div>
+                            <div class="section">
+                                <dl>
+                                    <dt>订单号：</dt>
+                                    <dd>2018070220185782655</dd>
+                                    <dt>订单来源：</dt>
+                                    <dd>PC</dd>
+                                </dl>
+                                <dl>
+                                    <dt>购货人：</dt>
+                                    <dd>chenyu</dd>
+                                    <dt>订单状态：</dt>
+                                    <dd>PC</dd>
+                                </dl>
+                                <dl>
+                                    <dt>支付方式：</dt>
+                                    <dd>银行汇款/转帐</dd>
+                                    <dt>配送方式：</dt>
+                                    <dd>顺丰速运</dd>
+                                </dl>
+                                <dl>
+                                    <dt>下单时间：</dt>
+                                    <dd>2018-07-02 20:18:01</dd>
+                                    <dt>付款时间：</dt>
+                                    <dd>未付款</dd>
+                                </dl>
+                                <dl>
+                                    <dt>发货时间：</dt>
+                                    <dd>未发货</dd>
+                                    <dt>发货单号：</dt>
+                                    <dd>未发货</dd>
+                                </dl>
+                                <dl>
+                                    <dt>自动确认收货时间：</dt>
+                                    <dd>15 天</dd>
+                                    <dt></dt>
+                                    <dd></dd>
+                                </dl>
+                            </div>
+                        </div>
+
+                        <div class="order-step clearfix mar-bt-50">
+                            <div class="step-title">
+                                <i class="ui-step"></i>
+                                <h3>操作信息</h3>
+                            </div>
+                            <div class="order-operation">
+
+                            </div>
                         </div>
                     </form>
                 </div>
