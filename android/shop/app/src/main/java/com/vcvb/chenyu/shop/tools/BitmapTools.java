9ec004b66388919fb0f0b4efb7fb0e35503c6ad4
chenyu00544 <chenyu00544@163.com>
2018-08-20 15:08:22 +0800

    update

--- android/shop/app/src/main/java/com/vcvb/chenyu/shop/tools/BitmapTools.java
+++ android/shop/app/src/main/java/com/vcvb/chenyu/shop/tools/BitmapTools.java
@@ -1,7 +1,17 @@
 package com.vcvb.chenyu.shop.tools;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import com.vcvb.chenyu.shop.R;
+
+import java.lang.ref.SoftReference;
 
 public class BitmapTools {
 
@@ -32,5 +42,85 @@ public class BitmapTools {
             inSimpleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
         }
         return inSimpleSize;
+    }
+
+    static class AsyncDrawable extends BitmapDrawable {
+        private final SoftReference<BitmapWorkerTask> softReference;
+
+        public AsyncDrawable(Resources resources, Bitmap bitmap, BitmapWorkerTask
+                bitmapWorkerTask) {
+            super(resources, bitmap);
+            softReference = new SoftReference<BitmapWorkerTask>(bitmapWorkerTask);
+        }
+
+        public BitmapWorkerTask getBitmapWorkerTask() {
+            return softReference.get();
+        }
+    }
+
+    static class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+        private SoftReference<ImageView> imageViewSoftReference;
+        private String data = "";
+
+        public BitmapWorkerTask(ImageView imageView) {
+            imageViewSoftReference = new SoftReference<ImageView>(imageView);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            data = strings[0];
+            byte[] result = HttpUtils.sendPost(data);
+            return BitmapTools.decodeBitmap(result, 100, 100);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (isCancelled()) {
+                bitmap = null;
+            }
+
+            if (imageViewSoftReference != null && bitmap != null) {
+                final ImageView imageView = imageViewSoftReference.get();
+                final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+                if (this == bitmapWorkerTask && imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+    }
+
+    private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
+        if (imageView != null) {
+            final Drawable drawable = imageView.getDrawable();
+            if (drawable instanceof AsyncDrawable) {
+                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+                return asyncDrawable.getBitmapWorkerTask();
+            }
+        }
+        return null;
+    }
+
+    public static boolean cancelPotntialWork(String data, ImageView imageView){
+        final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+        if(bitmapWorkerTask != null){
+            final String bitmapData = bitmapWorkerTask.data;
+            if(bitmapData != data){
+                bitmapWorkerTask.cancel(true);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void loadBitmap(Resources resources, String data, ImageView imageView){
+        Bitmap placeBitmap = BitmapFactory.decodeResource(resources, R.drawable.empty);
+        if(cancelPotntialWork(data, imageView)){
+            final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+            final AsyncDrawable asyncDrawable = new AsyncDrawable(resources, placeBitmap, task);
+            imageView.setImageDrawable(asyncDrawable);
+            task.execute(data);
+        }
     }
 }
